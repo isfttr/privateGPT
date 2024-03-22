5a89d6553bf7c05f572ed2d4adcb5b08b1c76dc4
isfttr <lucas.fernandes.df@gmail.com>
2024-03-22 15:57:49 -0300

    add: bash script to serve, build and run privateGPT

---
+++ run-privateGPT.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Change directory to the desired folder
+cd $HOME/isfttr-privateGPT
+
+# Start the server using "ollama serve" command
+echo "Starting Ollama server..."
+ollama serve &
+
+# Background the server process with PID $server_pid
+server_pid=$!
+
+# Start the build and run process using "PGPT_PROFILES=ollama make run" command
+echo "Starting build and run process..."
+ PGPT_PROFILES=ollama make run &
+
+# Start the server  on localhost:8001
+open http://127.0.0.1:8001/ &
+
+# Store the PID of the build and run process in a variable named $process_pid
+process_pid=$!
+
+# Wait for both processes to finish before exiting the script
+wait $server_pid $process_pid
